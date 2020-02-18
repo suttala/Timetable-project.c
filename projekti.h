c96b0f6e854e91c2b1797e60ce21fb54e49096b4
suttala <60404638+suttala@users.noreply.github.com>
2020-02-18 12:28:09 +0200

    Add files via upload
    The header file used in the project.

---
+++ projekti.h
@@ -0,0 +1,19 @@
+//
+//  projekti.h
+//  Ajanvaraus_projekti
+//
+//  Created by Alarik Sutter on 02/04/2019.
+//  Copyright © 2019 Alarik Sutter. All rights reserved.
+//
+
+#ifndef projekti_h
+#define projekti_h
+
+typedef struct {
+    char description[20];
+    int Month;
+    int Day;
+    int Time;
+}Schedule;
+
+#endif /* projekti_h */
